19234cef35d631384ffe5d3edddcfb7f70b220cb
Alex Taylor <alexetaylor7@gmail.com>
2024-03-26 13:09:40 -0600

    almost getting api to call

--- weather/src/app/home/home.page.ts
+++ weather/src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  cityName: string = "";
+
+  constructor(private weatherService: WeatherService) {}
+
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Boise')
+    .subscribe({
+      next: (response) => {
+        console.log(response);
+      }
+    })
+  }
+
+  getWeather() {
+    console.log(this.weatherService.getWeatherData(this.cityName));
+  }
 
 }
--- weather/src/app/services/weather.service.ts
+++ weather/src/app/services/weather.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
+  ApiKey = '085f41f3da4bb1804408481013545c75';
+
   constructor(private http: HttpClient) { }
 
 
   getWeatherData(cityName: string) {
-   // this.http.get()
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={this.ApiKey}')
   }
 }
